cc111e095e7918d7636c893d78d492e1ab9daabe
jcc <370234915@qq.com>
2020-10-24 01:06:28 +0800

    first

--- routes.php
+++ routes.php
@@ -2,7 +2,38 @@
 Route::any('wechat', function () {
     $app = app('wechat');
     $app->server->push(function ($message) {
-        return "您好！欢迎使用 EasyWeChat!";
+        \Log::info($message);
+        switch ($message['MsgType']) {
+            case 'event':
+                return '收到事件消息';
+                break;
+            case 'text':
+                return '收到文字消息';
+                break;
+            case 'image':
+                return '收到图片消息';
+                break;
+            case 'voice':
+                return '收到语音消息';
+                break;
+            case 'video':
+                return '收到视频消息';
+                break;
+            case 'location':
+                return '收到坐标消息';
+                break;
+            case 'link':
+                return '收到链接消息';
+                break;
+            case 'file':
+                return '收到文件消息';
+            // ... 其它消息
+            default:
+                return '收到其它消息';
+                break;
+        }
+
+        // ...
     });
     $response = $app->server->serve();
     return $response->send();
