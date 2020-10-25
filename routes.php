3af7883f5a2562b64b0523361ca6a394ec704711
jcc <370234915@qq.com>
2020-10-25 16:58:14 +0800

    微信扫码登录

--- routes.php
+++ routes.php
@@ -2,5 +2,5 @@
 
 
 Route::any('wechat','Jcc\Scanlogin\Http\Controllers\ScanloginController@wechat');
-Route::post('mini/login','Jcc\Scanlogin\Http\Controllers\ScanloginController@minLogin');
+Route::post('mini/login','Jcc\Scanlogin\Http\Controllers\ScanloginController@miniLogin');
 
